0f59c24ac5e1305f873136a82a1a9ce503f7290e
J-main <J-main@J-018>
2022-10-21 18:37:21 +0900

    dk

--- src/main/java/org/zerock/domain/CustomUser.java
+++ src/main/java/org/zerock/domain/CustomUser.java
@@ -17,7 +17,7 @@ import lombok.Getter;
 public class CustomUser extends User {
 	private static final long serialVersionUID = 1L;
 	private MemberVO member;	// 회원정보
-
+	
 	public CustomUser(String username, String password,
 			Collection<? extends GrantedAuthority> authorities) {
 		super(username, password, authorities);
