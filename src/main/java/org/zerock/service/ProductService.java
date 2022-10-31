de2c8ae54cce6536b44ebdec7b9f1b66ed8d9b64
Devehrbd <115767260+Devehrbd@users.noreply.github.com>
2022-10-31 12:47:28 +0900

    Update ProductService.java

--- src/main/java/org/zerock/service/ProductService.java
+++ src/main/java/org/zerock/service/ProductService.java
@@ -2,7 +2,6 @@ package org.zerock.service;
 
 import java.util.List;
 
-import org.zerock.domain.Bid_historyVO;
 import org.zerock.domain.ProductPicVO;
 import org.zerock.domain.ProductVO;
 
@@ -15,12 +14,10 @@ public interface ProductService {
 	public int currentPriceRead(int product_id);
 	public void currentPriceInsert(int product_id, int current_price,String user_id);
 	public String currentPriceUserRead(int product_id);
-	
-	
-	public List<Bid_historyVO> readBidList(String user_id);
+	public void productRegist(ProductVO product);
 	
 	// 아래 동길
-	public void register(ProductVO product);
+	
 	public ProductVO get(int product_id);
 	public boolean modify(ProductVO product);
 	public boolean remove(int product_id);
