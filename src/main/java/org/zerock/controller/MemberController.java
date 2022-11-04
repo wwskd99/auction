ca9fa7715e74ecbc6269dd372e1d08ab8c928b68
lee <wwskd99@daum.net>
2022-11-04 14:57:38 +0900

    로그인관련

--- src/main/java/org/zerock/controller/MemberController.java
+++ src/main/java/org/zerock/controller/MemberController.java
@@ -1,5 +1,8 @@
 package org.zerock.controller;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -15,55 +18,69 @@ import org.zerock.service.MemberService;
 import org.zerock.service.ProductService;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
 
+@Log4j
 @Controller
 @RequestMapping("/member/*")
 @AllArgsConstructor
 public class MemberController {
-	
-	private MemberService mService; 
+
+	private MemberService mService;
 	private ProductService pService;
-	
+
 	@PostMapping("/loginb")
-	public String memberLogin(@RequestParam("user_id") String user_id,@RequestParam("pw") String pw, HttpServletRequest request) {
-		
+	public String memberLogin(@RequestParam("user_id") String user_id, @RequestParam("pw") String pw,
+			HttpServletRequest request) {
+		HttpSession session = request.getSession();
+
+		log.info(user_id);
+		log.info(pw);
+
+		if (user_id == null || user_id == "") {
+			session.setAttribute("login_message", "아이디 넣어주세요");
+			return "member/login";
+		}
+		if (pw == null || pw == "") {
+			session.setAttribute("login_message", "비밀번호 넣어주세요");
+			return "member/login";
+		}
+
 		MemberVO mVo = mService.MemberRead(user_id);
-		
-		if(mVo.getUser_id().equals(user_id)) {
+		if (mVo == null) {
+			session.setAttribute("login_message", "해당하는 회원이 없습니다.");
+			return "member/login";
+		}
+		if (mVo.getUser_id().equals(user_id)) {
 			// id 일치
 
-			if(mVo.getPw().equals(pw)) {
-				//pw 일치
-				
-				HttpSession session = request.getSession();
+			if (mVo.getPw().equals(pw)) {
+				// pw 일치
+
 				session.setAttribute("sessionMember", mVo);
 				session.setAttribute("userid", mVo.getUser_id());
 				session.setAttribute("sessionUser", mVo.getUser_id());
 				return "main";
-				
-			}else {
-				//pw 불일치
-				System.out.println("로긴실패 pw불일치 - 나중에 printwriter 알아서");
-				
-				return "login";
+
+			} else {
+				// pw 불일치
+				session.setAttribute("login_message", "로그인 실패 패스워드 불일치");
+				return "member/login";
 			}
-			
+
 		} else {
-			//id 불일치
-			System.out.println("로긴 실패 해당하는 id 없음 - 나중에 printwirter 알아서");
-			
-			return "login";
+			// id 불일치
+			session.setAttribute("login_message", "로그인 실패 아이디 불일치");
+			return "member/login";
 		}
-		
-		
-		
+
 	}
-	
+
 	@RequestMapping("/login")
 	public void memberLogin() {
-		
+
 	}
-	
+
 	@RequestMapping("/logout")
 	@ResponseBody
 	public ModelAndView memberLogout(HttpServletRequest request) {
@@ -73,79 +90,123 @@ public class MemberController {
 		mv.setViewName("/main");
 		return mv;
 	}
-	
+
 	@RequestMapping("/join")
 	public void memberJoin() {
-		
+
 	}
-	
+
 	@PostMapping("/joinb")
-	public  ModelAndView memberJoin(@RequestParam("user_id") String user_id,@RequestParam("pw") String pw, @RequestParam("name") String name, @RequestParam("phone") String phone, @RequestParam("birth") String birth) {
-		MemberVO mVo = new MemberVO();
+	public ModelAndView memberJoin(@RequestParam("user_id") String user_id, @RequestParam("pw") String pw,
+			@RequestParam("name") String name, @RequestParam("phone") String phone, @RequestParam("birth") String birth,
+			HttpServletRequest request) {
+
+		MemberVO mVo = mService.MemberRead(user_id);
 		ModelAndView mv = new ModelAndView();
-		
-		mVo.setUser_id(user_id);
-		mVo.setPw(pw);
-		mVo.setName(name);
-		mVo.setPhone(phone);
-		mVo.setBirth(birth);
-		
-		mService.joinMember(mVo);
-		mv.addObject("message", "회원가입을 했습니다.");
-		mv.setViewName("/main");
-		return mv;
+		HttpSession session = request.getSession();
+		mv.setViewName("/member/join");
+
+		Pattern pattern = Pattern.compile("\\d{3}-\\d{4}-\\d{4}");
+		Matcher matcher = pattern.matcher(phone);
+
+		if (user_id == null || user_id == "") {
+			session.setAttribute("join_message", "아이디 넣어주세요.");
+			return mv;
+		} else if (pw == null || pw == "") {
+			session.setAttribute("join_message", "비밀번호 넣어주세요.");
+			return mv;
+		} else if (name == null || name == "") {
+			session.setAttribute("join_message", "이름 넣어주세요.");
+			return mv;
+		} else if (mVo != null) {
+			session.setAttribute("join_message", "존재하는 회원입니다.");
+			return mv;
+		} else if (!matcher.matches()) {
+			session.setAttribute("join_message", "유효하지 않는 전화번호입니다. ex: XXX-XXXX-XXX");
+			return mv;
+		} else {
+			MemberVO mVo1 = new MemberVO();
+			mVo1.setUser_id(user_id);
+			mVo1.setPw(pw);
+			mVo1.setName(name);
+			mVo1.setPhone(phone);
+			mVo1.setBirth(birth);
+
+			mService.joinMember(mVo1);
+			mv.addObject("message", "회원가입을 했습니다.");
+			mv.setViewName("/main");
+			return mv;
+		}
 	}
-	
+
 	@RequestMapping("/resign")
 	@ResponseBody
 	public ModelAndView memberResign(HttpServletRequest request) {
 		ModelAndView mv = new ModelAndView();
-		
+
 		HttpSession session = request.getSession();
-		String userid=(String)session.getAttribute("userid");
+		String userid = (String) session.getAttribute("userid");
 		int result = pService.IsExist(userid);
-		if(result == 1) {
+		if (result == 1) {
 			mv.addObject("message", "판매 게시글이 존재하여 불가능합니다.");
 			mv.setViewName("/main");
 			return mv;
 		} else {
-			
+
 			mService.deleteBid_history(userid);
-			mService.deleteTrade(userid);		
+			mService.deleteTrade(userid);
 			mService.resignMember(userid);
-			
+
 			session.invalidate();
 			mv.addObject("message", "탈퇴 처리를 했습니다.");
 			mv.setViewName("/main");
 			return mv;
 		}
 	}
-	
+
 	@RequestMapping("/update")
 	public ModelAndView memberUpdate(HttpServletRequest request) {
 		ModelAndView mv = new ModelAndView();
 		HttpSession session = request.getSession();
-		
+
 		mv.addObject("mVo", session.getAttribute("sessionMember"));
 		mv.setViewName("/member/update");
-		
-		return mv;	
+
+		return mv;
 	}
-	
+
 	@PostMapping("/updateb")
-	public  ModelAndView memberUpdate(@RequestParam("user_id") String user_id,@RequestParam("pw") String pw, @RequestParam("name") String name, @RequestParam("phone") String phone, @RequestParam("birth") String birth) {
+	public ModelAndView memberUpdate(@RequestParam("pw") String pw, @RequestParam("name") String name,
+			@RequestParam("phone") String phone, @RequestParam("birth") String birth, HttpServletRequest request) {
 		MemberVO mVo = new MemberVO();
 		ModelAndView mv = new ModelAndView();
-		
-		mVo.setUser_id(user_id);
-		mVo.setPw(pw);
-		mVo.setName(name);
-		mVo.setPhone(phone);
-		mVo.setBirth(birth);
+		HttpSession session = request.getSession();
+		String user_id = (String) session.getAttribute("sessionUser");
+		mv.setViewName("/member/update");
 
-		mService.updateMember(mVo);
-		mv.addObject("message", "회원정보를 수정했습니다.");
-		mv.setViewName("/main");
-		return mv;
+		Pattern pattern = Pattern.compile("\\d{3}-\\d{4}-\\d{4}");
+		Matcher matcher = pattern.matcher(phone);
+
+		if (pw == null || pw == "") {
+			session.setAttribute("update_message", "비밀번호 넣어주세요.");
+			return mv;
+		} else if (name == null || name == "") {
+			session.setAttribute("update_message", "이름 넣어주세요.");
+			return mv;
+		} else if (!matcher.matches()) {
+			session.setAttribute("update_message", "유효하지 않는 전화번호입니다. ex: XXX-XXXX-XXX");
+			return mv;
+		} else {
+			mVo.setUser_id(user_id);
+			mVo.setPw(pw);
+			mVo.setName(name);
+			mVo.setPhone(phone);
+			mVo.setBirth(birth);
+
+			mService.updateMember(mVo);
+			mv.addObject("message", "회원정보를 수정했습니다.");
+			mv.setViewName("/main");
+			return mv;
+		}
 	}
 }
--- src/main/webapp/WEB-INF/views/member/update.jsp
+++ src/main/webapp/WEB-INF/views/member/update.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,23 +12,28 @@
 		<table>
 			<tr>
 				<td>아이디</td>
-				<td><input type="text" name="user_id" placeholder="아이디" value="${mVo.user_id}" readonly></td>
+				<td><input type="text" name="user_id" placeholder="아이디"
+					value="${sessionScope.userid}" readonly></td>
 			</tr>
 			<tr>
 				<td>이름</td>
-				<td><input type="text" name="name" value="${mVo.name}" placeholder="이름"></td>
+				<td><input type="text" name="name" value="${mVo.name}"
+					placeholder="이름"></td>
 			</tr>
 			<tr>
 				<td>비밀번호</td>
-				<td><input type="password" name="pw" value="${mVo.pw}" placeholder="비밀번호"></td>
+				<td><input type="password" name="pw" value="${mVo.pw}"
+					placeholder="비밀번호"></td>
 			</tr>
 			<tr>
 				<td>전화번호</td>
-				<td><input type="tel" name="phone" value="${mVo.phone}" placeholder="전화번호"></td>
+				<td><input type="tel" name="phone" value="${mVo.phone}"
+					placeholder="전화번호"></td>
 			</tr>
 			<tr>
 				<td>생일</td>
-				<td><input type="date" name="birth" value="${mVo.birth}" placeholder="생일"></td>
+				<td><input type="date" name="birth" value="${mVo.birth}"
+					placeholder="생일"></td>
 			</tr>
 			<tr>
 				<td>
@@ -36,5 +42,10 @@
 			</tr>
 		</table>
 	</form>
+	<c:if test="${!empty update_message}">
+		<script>
+			alert("${update_message}")
+		</script>
+	</c:if>
 </body>
 </html>
